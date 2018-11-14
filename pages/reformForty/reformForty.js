4cd7e201978f495514c398f63e9d7a1d4ecc3a03
CNOliverZhang <cnoliverzhang@gmail.com>
2018-11-14 16:41:29 +0800

    加入改开

---
+++ pages/reformForty/reformForty.js
@@ -0,0 +1,87 @@
+// pages/Aaa/Aaa.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+  //进入答题区域
+  goToQues: function () {
+    wx.navigateTo({
+      url: '../Answer/Answer',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    return {
+      title: "改革开放小问卷",
+      success: function (res) {
+        wx.showToast({
+          title: "分享成功",
+          icon: 'success',
+          duration: 2000
+        })
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: "分享失败",
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    }
+  }
+})
